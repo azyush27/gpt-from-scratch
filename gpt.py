b1c9179486da2480ca9c1c6d0d67ab63b0925bf5
azyush27 <ayush27pakhale@gmail.com>
2026-06-29 18:32:28 +0530

    Your commit message

--- gpt.py
+++ gpt.py
@@ -33,3 +33,42 @@ print( data [:1000]) # the 1000 characters we looked at earlier will go to the g
 n = int(0.9*len(data))
 train_data = data[:n]
 val_data = data[n:]
+
+block_size = 8 
+train_data [:block_size+1]
+print(train_data)
+
+x = train_data[:block_size]
+y= train_data[1:block_size+1]
+for t in range(block_size ):
+    context = x[:t+1]
+    target = y[t]
+    print(f"when input is {context} then the output is : {target}")
+
+torch.manual_seed(1337)              
+batch_size  = 4 # how many parallel sequneces will we process altogether at once
+block_size = 8  # what is the maximum context length for predictions 
+
+def get_batch(split):
+    data = train_data if split == "train" else val_data
+    ix = torch.randint(len(data)-block_size,(batch_size,))
+    x = torch.stack([data[i:i+block_size] for i in ix])
+    y = torch.stack([data[i+1:i+block_size+1] for i in ix])
+    return x,y
+
+xb,yb = get_batch('train')
+print("inputs : ")
+print(xb.shape)
+print(xb)
+print("targets :")
+      
+print(yb.shape)
+print(yb)
+
+for b in range (batch_size) : # batch dimension
+    for t in range (block_size): #block dimension
+        context = xb[b, :t+1]
+        target = yb [b,t]
+        print(f"when input is {context.tolist()} the target is {target}")
+
+
