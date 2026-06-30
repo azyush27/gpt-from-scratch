85cac5a3de1a229fd6c74bcd73735246b6f75bbb
azyush27 <ayush27pakhale@gmail.com>
2026-06-30 23:55:07 +0530

    Your commit message here

--- gpt.py
+++ gpt.py
@@ -72,3 +72,55 @@ for b in range (batch_size) : # batch dimension
         print(f"when input is {context.tolist()} the target is {target}")
 print("hello")
 
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+torch.manual_seed(1337)
+
+class Bigramlanguagemodel(nn.Module):
+    def __init__(self,vocab_size):
+        super(). __init__()
+        #each directly reads off the logits for the next token from a lookup table 
+        self.token_embedding_table = nn.Embedding(vocab_size , vocab_size)
+
+    def forward(self,idx,targets=None):
+            #idx and targets are both (B,T ) tensors of integers # for example if b t = xb,yb then the tensors dimensions will be (4,8)
+            logits = self.token_embedding_table(idx) #B,T,C,
+            if targets is None :
+                 loss = None 
+            else :     
+             B,T,C = logits.shape 
+             logits = logits.view(B*T , C)
+             targets = targets.view(B*T)
+
+             loss = F.cross_entropy(logits,targets)
+            return logits , loss
+    
+    def generate (self,idx,max_new_tokens):
+        #idx is (VB,T ) array of indices in the current context
+        for _ in range (max_new_tokens):
+            #get the predictions 
+            logits,loss = self(idx)
+            #focus only on the last time step
+            logits = logits[:,-1,:] # becomes (B,C)
+            #apply softmax to get probabilities
+            probs = F.softmax (logits,dim = -1) # (B,C)
+            #sample from the distribution 
+            idx_next = torch.multinomial (probs , num_samples = 1 ) # (B,1)
+            #append sampled index to the running sequence 
+            idx = torch.cat((idx,idx_next) , dim = 1) # (B , T+1)
+        return idx    
+
+
+        
+
+        
+m = Bigramlanguagemodel(vocab_size)
+logits,loss = m(xb,yb) # this is same as out = m.forward(xb,yb)
+print(logits.shape)
+print(loss)
+
+idx = torch.zeros((1,1) , dtype = torch.long )
+print(decode(m.generate(idx = torch.zeros((1,1),dtype=torch.long) , max_new_tokens = 100) [0].tolist() ))
+
+
