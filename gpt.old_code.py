d0a27b155854975c141d6b3a71745e3ff33db1dd
azyush27 <ayush27pakhale@gmail.com>
2026-07-06 20:41:36 +0530

    added the great single - self attention block

--- gpt.old_code.py
+++ gpt.old_code.py
@@ -83,6 +83,7 @@ class Bigramlanguagemodel(nn.Module):
     def __init__(self,vocab_size):
         super(). __init__()
         #each directly reads off the logits for the next token from a lookup table 
+        
         self.token_embedding_table = nn.Embedding(vocab_size , vocab_size)
 
     def forward(self,idx,targets=None):
